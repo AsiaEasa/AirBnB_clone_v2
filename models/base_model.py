f3dd062abe3f03055f023898de11b14d7fa05f73
Amedamin <ahmedqryqr@gmail.com>
2024-03-16 10:59:56 -0700

    done

--- models/base_model.py
+++ models/base_model.py
@@ -10,32 +10,20 @@ class BaseModel:
 
     "The INIT method"
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)  # <-- Ensure storage.new() is called correctly
+            storage.new(self)
         else:
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                kwargs['created_at'] = datetime.now()
-            else:
-                if isinstance(kwargs['created_at'], str):
-                    kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                             '%Y-%m-%dT%H:%M:%S.%f')
-
-            if 'updated_at' not in kwargs:
-                kwargs['updated_at'] = datetime.now()
-            else:
-                if isinstance(kwargs['updated_at'], str):
-                    kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                             '%Y-%m-%dT%H:%M:%S.%f')
-
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-
             self.__dict__.update(kwargs)
 
     def save(self):
