bcf39df335a0fa9c99fa5ac831dee34851d410aa
AsiaEasa <asiaeasa89@gmail.com>
2024-03-17 15:44:03 -0700

    done

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
-ST= getenv("HBNB_TYPE_STORAGE")
-if ST == "db":
-    storage = DBStorage()
-else:
-    storage = FileStorage()
+
+storage = FileStorage()
 storage.reload()
--- models/city.py
+++ models/city.py
@@ -3,14 +3,11 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 import models
-from models import ST
+from sqlalchemy import ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if ST == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    else:
-        name = ''
-        state_id = ''
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
