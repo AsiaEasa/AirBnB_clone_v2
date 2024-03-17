e73fd86cc3e963da961cdf088b9ad86f46972adb
Amedamin <ahmedqryqr@gmail.com>
2024-03-17 15:30:48 -0700

    done

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,3 +11,8 @@ from models.user import User
 from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
+
+class DBStorage:
+    """ create tables in environmental"""
+    __engine = None
+    __session = None
