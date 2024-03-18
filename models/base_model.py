dcb134f5ed12a6a85b9bbea7d4d71e5af1ac48ee
Amedamin <ahmedqryqr@gmail.com>
2024-03-18 13:25:30 -0700

    done

--- models/base_model.py
+++ models/base_model.py
@@ -12,32 +12,31 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
+        IOS = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs and kwargs != {}:
+            for K, V in kwargs.items():
+                if K == "created_at" or K == "updated_at":
+                    self.__dict__[K] = datetime.strptime(V, IOS)
+                else:
+                    if K != "__class__":
+                        setattr(self, K, V)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
-            self.created_at = datetime.strptime(kwargs.get(
-                    'created_at', datetime.now().strftime(
-                     '%Y-%m-%dT%H:%M:%S.%f')), '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(kwargs.get(
-                    'updated_at', datetime.now().strftime(
-                     '%Y-%m-%dT%H:%M:%S.%f')), '%Y-%m-%dT%H:%M:%S.%f')
-            for key, value in kwargs.items():
-                if key not in ['created_at', 'updated_at']:
-                    setattr(self, key, value)
 
     def __str__(self):
-        """ To handle print()
-        """
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """ UPDATE the public instance attribute updated_at
