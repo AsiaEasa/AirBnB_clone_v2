7c63093006f844f2af4480bb8ff85d79a50e452a
Amedamin <ahmedqryqr@gmail.com>
2024-03-18 03:40:02 -0700

    done

--- models/state.py
+++ models/state.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
-from models.city import City
-import shlex
-from models import storage
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-    @property
-    def cities(self):
-        """Getter attribute that returns the list of City instances with state_id equal to the current State.id"""
-        from models import storage
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    name = ""
