2422e31d34a05f82818fead8c4aaf062666409ab
AsiaEasa <asiaeasa89@gmail.com>
2024-04-03 12:02:19 -0700

    done

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""generates a .tgz archive"""
+import os
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Function do pack"""
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    N = datetime.now()
+    F = (
+        f"versions/web_static_{N.strftime('%Y%m%d_%H%M%S')}"
+        + ".tgz"
+    )
+    local(f"tar -cvzf {F} web_static")
+
+    if os.path.exists(FN):
+        return FN
+    else:
+        return None
