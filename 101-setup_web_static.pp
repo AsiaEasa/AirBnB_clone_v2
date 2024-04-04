5c201fc2051424bda4821d80032d831ebabd6e65
AsiaEasa <asiaeasa89@gmail.com>
2024-04-04 15:16:09 -0700

    done

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,42 +1,48 @@
-exec { 'apt-update-upgrade':
-  command => 'apt-get update && apt-get upgrade -y',
-  path    => '/usr/bin',
+# File: 101-setup_web_static.pp
+
+exec { 'apt-get-update':
+	command => 'apt-get update',
+		path    => '/usr/bin',
+}
+
+exec { 'apt-get-upgrade':
+	command => 'apt-get upgrade -y',
+		path    => '/usr/bin',
+		require => Exec['apt-get-update'],
 }
 
 package { 'apache2':
   ensure => installed,
 }
 
-file { ['/data/web_static', '/data/web_static/releases', '/data/web_static/shared']:
+file { '/data/web_static':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases':
   ensure => 'directory',
   owner  => 'root',
   group  => 'root',
 }
 
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '644',
+file { '/data/web_static/shared':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
 }
 
 file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test',
-  owner   => 'root',
-  group   => 'root',
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+  owner  => 'root',
+  group  => 'root',
   require => File['/data/web_static/releases/test/index.html'],
 }
 
 service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => [Package['apache2'], File['/data/web_static/current']],
+  ensure => running,
+  enable => true,
+  require => Package['apache2'],
 }
