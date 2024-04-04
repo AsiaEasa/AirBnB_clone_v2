b6c43425f870ed6e023a8703aa4bcb1c4b7441fd
AsiaEasa <asiaeasa89@gmail.com>
2024-04-04 14:12:26 -0700

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,25 +5,27 @@ Fabric script (based on the file 1-pack_web_static.py)
 from fabric.api import *
 import os
 
+env.user = 'ubuntu'
 env.hosts = ["54.197.107.140", "54.166.14.2"]
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive"""
+    if not os.path.exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        AF = os.path.basename(archive_path)
+        AN = os.path.splitext(AF)[0]
+        AP = f"/data/web_static/releases/{AN}"
+        run(f"mkdir -p {AP}")
+        run(f"tar -xzf /tmp/{AF} -C {AP}")
+        run(f"rm /tmp/{AF}")
+        run(f"mv {AP}/web_static/* {AP}")
+        run(f"rm -rf {AP}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {AP} /data/web_static/current")
+        print("New version deployed!")
         return True
-    except:
+    except Exception as e:
         return False
