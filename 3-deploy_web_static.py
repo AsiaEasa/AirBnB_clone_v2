1ad7cc2021755107ffb4a05a3aabcd18bc7e26e7
AsiaEasa <asiaeasa89@gmail.com>
2024-04-04 14:18:15 -0700

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -26,22 +26,21 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive"""
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        AF = os.path.basename(archive_path)
-        AN = os.path.splitext(AF)[0]
-        AP = f"/data/web_static/releases/{AN}"
-        run(f"mkdir -p {AP}")
-        run(f"tar -xzf /tmp/{AF} -C {AP}")
-        run(f"rm /tmp/{AF}")
-        run(f"mv {AP}/web_static/* {AP}")
-        run(f"rm -rf {AP}/web_static")
-        run(f"rm -rf /data/web_static/current")
-        run(f"ln -s {AP} /data/web_static/current")
-        print("New version deployed!")
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except Exception as e:
         return False
