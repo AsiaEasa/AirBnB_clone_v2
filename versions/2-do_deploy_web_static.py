e1a804d1e86f8ea3790b553365cd27ab82ec01b4
AsiaEasa <asiaeasa89@gmail.com>
2024-04-04 10:26:40 -0700

    done

---
+++ versions/2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 1-pack_web_static.py)
+"""
+from fabric.api import *
+import os
+
+
+env.user = 'ubuntu'
+env.hosts = ["54.197.107.140", "54.166.14.2"]
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to your web servers
+    """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        put(archive_path, '/tmp/')
+        AF = os.path.basename(archive_path)
+        AN = os.path.splitext(AF)[0]
+        AP = f"/data/web_static/releases/{AN}"
+        run(f"mkdir -p {AP}")
+        run(f"tar -xzf /tmp/{AF} -C {AP}")
+        run(f"rm /tmp/{AF}")
+        run(f"mv {AP}/web_static/* {AP}")
+        run(f"rm -rf {AP}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {AP} /data/web_static/current")
+        print("New version deployed!")
+        return True
+    except:
+        return False
