263e97be3a42456e68b11b9d0c8ba11d2b1fdeba
AsiaEasa <asiaeasa89@gmail.com>
2024-04-04 14:21:39 -0700

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,39 +8,37 @@ env.hosts = ["54.197.107.140", "54.166.14.2"]
 
 
 def do_pack():
-    """Function do pack"""
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-
-    N = datetime.now()
-    F = (
-        f"versions/web_static_{N.strftime('%Y%m%d_%H%M%S')}"
-        + ".tgz"
-    )
-    local(f"tar -cvzf {F} web_static")
-
-    if os.path.exists(F):
-        return F
-    else:
+    """
+    Generates a tgz archive in the web_static folder
+    """
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        archive = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(archive))
+        return archive
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive"""
+    if not os.path.exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        AF = os.path.basename(archive_path)
+        AN = os.path.splitext(AF)[0]
+        AP = f"/data/web_static/releases/{AN}"
+        run(f"mkdir -p {AP}")
+        run(f"tar -xzf /tmp/{AF} -C {AP}")
+        run(f"rm /tmp/{AF}")
+        run(f"mv {AP}/web_static/* {AP}")
+        run(f"rm -rf {AP}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {AP} /data/web_static/current")
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
